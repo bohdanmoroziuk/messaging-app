59fe9085d7132b54cfea395307f5a5a6b5048657
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-06-11 10:01:46 +0300

    feat: render a list of messages

--- App.tsx
+++ App.tsx
@@ -1,13 +1,38 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
 import Status from 'src/components/Status';
+import MessageList from 'src/components/MessageList';
+
+import {
+  createTextMessage,
+  createImageMessage,
+  createLocationMessage,
+} from 'src/utils/messages';
 
 import styles from './App.styles';
 
 export default function App() {
+  const [messages, setMessages] = useState([
+    createImageMessage('https://unsplash.it/300/300'),
+    createTextMessage('Hello'),
+    createTextMessage('World'),
+    createLocationMessage({
+      latitude: 37.78825,
+      longitude: -122.4324,
+    }),
+  ]);
+
+  const handlePressMessage = () => {};
+
   const renderMessageList = () => (
-    <View style={styles.messageList}></View>
+    <View style={styles.messageList}>
+      <MessageList
+        messages={messages}
+        onPressMessage={handlePressMessage}
+      />
+    </View>
   );
 
   const renderInputMethodEditor = () => (
