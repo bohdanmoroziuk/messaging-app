af16a51aa13664a5d3ddc4f4fbeaf6cac8bda667
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-06-12 14:57:25 +0300

    chore: setup jest and testing library

---
+++ src/components/MessageList/MessageList.test.tsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react-native';
+
+import { MessageList } from './MessageList';
+import { createTextMessage, createImageMessage, createLocationMessage } from '../../utils/messages';
+import { Message } from '../../types';
+
+describe('MessageList', () => {
+  const mockMessages: Message[] = [
+    createImageMessage('https://unsplash.it/300/300'),
+    createTextMessage('Hello'),
+    createTextMessage('World'),
+    createLocationMessage({
+      latitude: 37.78825,
+      longitude: -122.4324,
+    }),
+  ];
+
+  const mockOnPressMessage = jest.fn();
+
+  it('renders successfully when the message list is empty', () => {
+    const { toJSON } = render(
+      <MessageList
+        messages={[]}
+        onPressMessage={mockOnPressMessage}
+      />
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders successfully when the message list contains items', () => {
+    const { toJSON } = render(
+      <MessageList
+        messages={mockMessages}
+        onPressMessage={mockOnPressMessage}
+      />
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
