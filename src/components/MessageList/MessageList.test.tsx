345477e36af4828671bc75f6077fa81004305957
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-06-20 07:59:47 +0300

    test(message-list): update tests

--- src/components/MessageList/MessageList.test.tsx
+++ src/components/MessageList/MessageList.test.tsx
@@ -1,10 +1,14 @@
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 
 import { MessageList } from './MessageList';
 import { createTextMessage, createImageMessage, createLocationMessage } from '../../utils/messages';
 import { Message } from '../../types';
 
 describe('MessageList', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   const mockMessages: Message[] = [
     createImageMessage('https://unsplash.it/300/300'),
     createTextMessage('Hello'),
@@ -37,5 +41,22 @@ describe('MessageList', () => {
     );
 
     expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('calls the onPressMessage handler when a list item is pressed', () => {
+    const { getByText } = render(
+      <MessageList
+        messages={mockMessages}
+        onPressMessage={mockOnPressMessage}
+      />
+    );
+
+    const message = getByText('Hello');
+
+    expect(message).toBeDefined();
+
+    fireEvent.press(message);
+
+    expect(mockOnPressMessage).toHaveBeenCalled();
   });
 });
